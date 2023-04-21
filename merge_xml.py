d79f58da8d27f81e23ab5094ac4d909c8373e1c6
Martin Tracey <matracey@outlook.ie>
2023-04-20 20:14:12 -0700

    Initial commit

---
+++ merge_xml.py
@@ -0,0 +1,13 @@
+"""
+Merge two XML files based on join properties and optionally output the merged data to a new XML file.
+"""
+
+
+def main() -> None:
+    """
+    Main function
+    """
+
+
+if __name__ == '__main__':
+    main()
